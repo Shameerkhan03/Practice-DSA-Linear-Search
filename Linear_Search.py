426883abc11f84d6e203399e02bdff872655c547
Shameerkhan03 <shameerrr03@gmail.com>
2023-03-17 19:45:32 +0500

    Added a dict

--- Linear_Search.py
+++ Linear_Search.py
@@ -2,3 +2,13 @@
 # create a dict which has two keys. 1- inputs 2- output
 # input key holds 2 more keys. 1- list of cards 2- the card we are searchng for
 # output key holds the position of the card we are searching
+
+test: dict
+{
+    "inputs":
+    {
+        "cards":[2,4,3,5,6,0],
+        "query":5
+    },
+    "output":3
+}
