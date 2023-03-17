8ab931e4b5a8c9014e93c255b65586386fb0a49a
Shameerkhan03 <shameerrr03@gmail.com>
2023-03-17 19:39:23 +0500

    Added some comments about the approach

---
+++ Linear_Search.py
@@ -0,0 +1,4 @@
+# how to search for a specific card in a deck of multiple cards 
+# create a dict which has two keys. 1- inputs 2- output
+# input key holds 2 more keys. 1- list of cards 2- the card we are searchng for
+# output key holds the position of the card we are searching
