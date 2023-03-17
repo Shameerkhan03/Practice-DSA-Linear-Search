cebb0260794f25a8d3336411df80408e6d665339
Shameerkhan03 <shameerrr03@gmail.com>
2023-03-17 20:38:01 +0500

    Appended list

--- Linear_Search.py
+++ Linear_Search.py
@@ -36,3 +36,77 @@ def search_card(cards, query) -> int:
 
 result = (search_card(test["inputs"]["cards"], test["inputs"]["query"])) == test["output"]
 print(result)
+
+# adding more dict to the list
+
+# query occurs in the middle
+tests.append(test)
+
+tests.append({
+    'input': {
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+        'query': 1
+    },
+    'output': 6
+})
+
+# query is the first element
+tests.append({
+    'input': {
+        'cards': [4, 2, 1, -1],
+        'query': 4
+    },
+    'output': 0
+})
+
+# query is the last element
+tests.append({
+    'input': {
+        'cards': [3, -1, -9, -127],
+        'query': -127
+    },
+    'output': 3
+})
+
+tests.append({
+    'input': {
+        'cards': [6],
+        'query': 6
+    },
+    'output': 0 
+})
+
+# cards does not contain query 
+tests.append({
+    'input': {
+        'cards': [9, 7, 5, 2, -9],
+        'query': 4
+    },
+    'output': -1
+})
+
+# cards is empty
+tests.append({
+    'input': {
+        'cards': [],
+        'query': 7
+    },
+    'output': -1
+})
+
+tests.append({
+    'input': {
+        'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
+        'query': 3
+    },
+    'output': 7
+})
+
+# query occurs multiple times
+tests.append({
+    'input': {
+        'cards': [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
+        'query': 6
+    },
+    'output': 2
+})
